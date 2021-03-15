706341fe495c34ce57190976369f6a85694624d9
Fernanda Landim <gran0467@algonquinlive.com>
2021-03-14 22:32:20 -0400

    Course content was added to html

--- app.js
+++ app.js
@@ -1,138 +1,175 @@
-const careers = [
+const courses = [
     // Level 1 courses
     {
         index: 0,
         name: 'Communications I',
         code: 'ENL1813M',
-        level: 1
+        level: 1,
+        career: ['design', 'video', 'motion', 'pm']
     },
     {
         index: 1,
         name: 'Introduction to Photography',
         code: 'MTM6101',
-        level: 1
+        level: 1,
+        career: ['video', 'motion']
     },
     {
         index: 2,
         name: 'Responsive Web Design I',
         code: 'MTM6130',
-        level: 1
+        level: 1,
+        career: ['design', 'front', 'back']
     },
     {
         index: 3,
         name: 'Digital Graphics',
         code: 'MTM6160',
-        level: 1
+        level: 1,
+        career: ['design', 'motion']
     },
     {
         index: 4,
         name: 'Design Foundations',
         code: 'MTM6162',
-        level: 1
+        level: 1,
+        career: ['design', 'video', 'front', 'motion', 'pm']
     },
     {
         index: 5,
         name: 'Design Thinking',
         code: 'DSN2100M',
-        level: 1
+        level: 1,
+        career: ['design', 'pm']
     },
     {
         index: 6,
         name: 'Entrepreneurial Mindset',
         code: 'GED0020',
-        level: 1
+        level: 1,
+        career: ['design', 'pm']
     },
     // Level 2 courses
     {
         index: 7,
         name: 'Communications II',
         code: 'ENL6007',
-        level: 2
+        level: 2,
+        career: ['design', 'video', 'motion', 'pm']
     },
     {
         index: 8,
         name: 'Video Foundations',
         code: 'MTM6200',
-        level: 2
+        level: 2,
+        career: ['video', 'motion', 'pm']
     },
     {
         index: 9,
         name: 'Content Management Systems I',
         code: 'MTM6231',
-        level: 2
+        level: 2,
+        career: ['front', 'back']
     },
     {
         index: 10,
         name: 'Responsive Web Design II',
         code: 'MTM6201',
-        level: 2
+        level: 2,
+        career: ['design', 'front', 'back']
     },
     {
         index: 11,
         name: 'User Experience Design',
         code: 'MTM6260',
-        level: 2
+        level: 2,
+        career: ['design', 'video', 'front', 'motion', 'pm']
     },
     // Level 3 courses
     {
         index: 12,
         name: 'Motion Graphics I',
         code: 'MTM6301',
-        level: 3
+        level: 3,
+        career: ['video', 'motion']
     },
     {
         index: 13,
         name: 'Introduction to JavaScript',
         code: 'MTM6302',
-        level: 3
+        level: 3,
+        career: ['front', 'back']
     },
     {
         index: 14,
         name: 'Content Management Systems II',
         code: 'MTM6303',
-        level: 3
+        level: 3,
+        career: ['front', 'back']
     },
     {
         index: 15,
         name: 'Media Project Planning',
         code: 'MTM6304',
-        level: 3
+        level: 3,
+        career: ['design', 'video', 'pm']
     },
     // Level 4 courses
     {
         index: 16,
         name: 'Applied Projects',
         code: 'MTM6460',
-        level: 4
+        level: 4,
+        career: ['design', 'video', 'front', 'back', 'motion', 'pm']
     },
     {
         index: 17,
         name: 'Digital Portfolio',
         code: 'MTM6461',
-        level: 4
+        level: 4,
+        career: ['design', 'video', 'front', 'back', 'motion', 'pm']
     },
     {
         index: 18,
         name: 'Video Production',
         code: 'MTM6402',
-        level: 4
+        level: 4,
+        career: ['video']
     },
     {
         index: 19,
         name: 'Motion Graphics II',
         code: 'MTM6403',
-        level: 4
+        level: 4,
+        career: ['motion']
     },
     {
         index: 20,
         name: 'Web Production',
         code: 'MTM6404',
-        level: 4
+        level: 4,
+        career: ['front']
     },
     {
         index: 21,
         name: 'Server-side Web Development',
         code: 'MTM6405',
-        level: 4
+        level: 4,
+        career: ['back']
     }
 ]
+
+const app = Vue.createApp({
+    data: function () {
+        return {
+            careers: ['UI & UX Designer', 'Video Producer', 'Front-end Developer', 'Back-end Developer', 'Motion Graphics Designer', 'Project Manager'],
+            courses: courses,
+            sort: 'career',
+        }
+    },
+    methods: {
+        
+    }
+})
+
+const vm = app.mount("#app");
