92b451af15fdf5643c94e6ffafef9de9461ac277
Fernanda Landim <gran0467@algonquinlive.com>
2021-05-28 21:03:15 -0400

    reupload of codes

--- app.js
+++ app.js
@@ -5,49 +5,49 @@ const courses = [
         name: 'Communications I',
         code: 'ENL1813M',
         level: 1,
-        id: ['design', 'video', 'motion', 'pm']
+        career: ['design', 'video', 'motion', 'pm']
     },
     {
         index: 1,
         name: 'Introduction to Photography',
         code: 'MTM6101',
         level: 1,
-        id: ['video', 'motion']
+        career: ['video', 'motion']
     },
     {
         index: 2,
         name: 'Responsive Web Design I',
         code: 'MTM6130',
         level: 1,
-        id: ['design', 'front', 'back']
+        career: ['design', 'front', 'back']
     },
     {
         index: 3,
         name: 'Digital Graphics',
         code: 'MTM6160',
         level: 1,
-        id: ['design', 'motion']
+        career: ['design', 'motion']
     },
     {
         index: 4,
         name: 'Design Foundations',
         code: 'MTM6162',
         level: 1,
-        id: ['design', 'video', 'front', 'motion', 'pm']
+        career: ['design', 'video', 'front', 'motion', 'pm']
     },
     {
         index: 5,
         name: 'Design Thinking',
         code: 'DSN2100M',
         level: 1,
-        id: ['design', 'pm']
+        career: ['design', 'pm']
     },
     {
         index: 6,
         name: 'Entrepreneurial Mindset',
         code: 'GED0020',
         level: 1,
-        id: ['design', 'pm']
+        career: ['design', 'pm']
     },
     // Level 2 courses
     {
@@ -55,35 +55,35 @@ const courses = [
         name: 'Communications II',
         code: 'ENL6007',
         level: 2,
-        id: ['design', 'video', 'motion', 'pm']
+        career: ['design', 'video', 'motion', 'pm']
     },
     {
         index: 8,
         name: 'Video Foundations',
         code: 'MTM6200',
         level: 2,
-        id: ['video', 'motion', 'pm']
+        career: ['video', 'motion', 'pm']
     },
     {
         index: 9,
         name: 'Content Management Systems I',
         code: 'MTM6231',
         level: 2,
-        id: ['front', 'back']
+        career: ['front', 'back']
     },
     {
         index: 10,
         name: 'Responsive Web Design II',
         code: 'MTM6201',
         level: 2,
-        id: ['design', 'front', 'back']
+        career: ['design', 'front', 'back']
     },
     {
         index: 11,
         name: 'User Experience Design',
         code: 'MTM6260',
         level: 2,
-        id: ['design', 'video', 'front', 'motion', 'pm']
+        career: ['design', 'video', 'front', 'motion', 'pm']
     },
     // Level 3 courses
     {
@@ -91,28 +91,28 @@ const courses = [
         name: 'Motion Graphics I',
         code: 'MTM6301',
         level: 3,
-        id: ['video', 'motion']
+        career: ['video', 'motion']
     },
     {
         index: 13,
         name: 'Introduction to JavaScript',
         code: 'MTM6302',
         level: 3,
-        id: ['front', 'back']
+        career: ['front', 'back']
     },
     {
         index: 14,
         name: 'Content Management Systems II',
         code: 'MTM6303',
         level: 3,
-        id: ['front', 'back']
+        career: ['front', 'back']
     },
     {
         index: 15,
         name: 'Media Project Planning',
         code: 'MTM6304',
         level: 3,
-        id: ['design', 'video', 'pm']
+        career: ['design', 'video', 'pm']
     },
     // Level 4 courses
     {
@@ -120,42 +120,42 @@ const courses = [
         name: 'Applied Projects',
         code: 'MTM6460',
         level: 4,
-        id: ['design', 'video', 'front', 'back', 'motion', 'pm']
+        career: ['design', 'video', 'front', 'back', 'motion', 'pm']
     },
     {
         index: 17,
         name: 'Digital Portfolio',
         code: 'MTM6461',
         level: 4,
-        id: ['design', 'video', 'front', 'back', 'motion', 'pm']
+        career: ['design', 'video', 'front', 'back', 'motion', 'pm']
     },
     {
         index: 18,
         name: 'Video Production',
         code: 'MTM6402',
         level: 4,
-        id: ['video']
+        career: ['video']
     },
     {
         index: 19,
         name: 'Motion Graphics II',
         code: 'MTM6403',
         level: 4,
-        id: ['motion']
+        career: ['motion']
     },
     {
         index: 20,
         name: 'Web Production',
         code: 'MTM6404',
         level: 4,
-        id: ['front']
+        career: ['front']
     },
     {
         index: 21,
         name: 'Server-side Web Development',
         code: 'MTM6405',
         level: 4,
-        id: ['back']
+        career: ['back']
     }
 ]
 
